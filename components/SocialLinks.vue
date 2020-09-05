4d55d0262b147b35593df80a404611cbbf07ac03
givanov <givanov@aligntech.com>
2020-09-05 22:40:58 +0300

    Social links

--- components/SocialLinks.vue
+++ components/SocialLinks.vue
@@ -5,23 +5,23 @@
         <div class="col-xl-10 offset-xl-1">
           <div class="social-link-warp">
             <div class="social-links">
-              <a href>
+              <!-- <a href>
                 <i class="fa fa-pinterest"></i>
               </a>
               <a href>
                 <i class="fa fa-linkedin"></i>
-              </a>
-              <a href>
+              </a> -->
+              <a href="https://www.instagram.com/elena.ivanova.rielter/" target="__blank">
                 <i class="fa fa-instagram"></i>
               </a>
-              <a href>
+              <a href="https://www.facebook.com/profile.php?id=100001570811983" target="__blank">
                 <i class="fa fa-facebook"></i>
               </a>
-              <a href>
+              <!-- <a href>
                 <i class="fa fa-twitter"></i>
-              </a>
+              </a> -->
             </div>
-            <h2 class="hidden-md hidden-sm">My Social Profiles</h2>
+            <h2 class="hidden-md hidden-sm">Социальные сети</h2>
           </div>
         </div>
       </div>
