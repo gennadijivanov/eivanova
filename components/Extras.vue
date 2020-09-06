0c76d7f59811a132e4e2bd8e0dee3dda880bae30
givanov <givanov@aligntech.com>
2020-09-06 22:41:27 +0300

    pug extras

--- components/Extras.vue
+++ components/Extras.vue
@@ -1,71 +1,43 @@
-<template>
-  <section class="extra-section spad pb-0">
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-xl-8 offset-xl-2">
-            <div class="section-title">
-              <h2>Дополнительно</h2>
-            </div>
-
-            <div class="row">
-              <div class="col-lg-3 col-md-6 pt-5">
-                <div class="fact-box trans">
-                  <div class="fact-content">
-                    <div class="circle-progress">
-                      <div id="progress1" class="prog-circle"></div>
-                      <div class="progress-info">
-                        <h2>100%</h2>
-                      </div>
-                      <div class="prog-title">
-                        <h3>Коммуникабельна</h3>
-                        <p>Найду общий язык с бомжом и Папой Римским</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div class="col-lg-3 col-md-6 pt-5">
-                <div class="fact-box trans">
-                  <div class="fact-content">
-                    <div class="circle-progress">
-                      <div id="progress2" class="prog-circle"></div>
-                      <div class="progress-info">
-                        <h2>100%</h2>
-                      </div>
-                      <div class="prog-title">
-                        <h3>Великолепна</h3>
-                        <p>а ещё скромна</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div class="col-lg-3 col-md-6">
-                <div class="fact-box">
-                  <div class="fact-content">
-                    <img src="img/icon/1-w.png" alt />
-                    <h2>14</h2>
-                    <p>Лет опыта</p>
-                  </div>
-                </div>
-              </div>
-
-              <div class="col-lg-3 col-md-6">
-                <div class="fact-box">
-                  <div class="fact-content">
-                    <img src="img/icon/2-w.png" alt />
-                    <h2>999</h2>
-                    <p>Наград</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
+<template lang="pug">
+section.extra-section.spad.pb-0
+  .container-fluid
+    .row
+      .col-xl-8.offset-xl-2
+        .section-title
+          h2 Дополнительно
+        .row
+          .col-lg-3.col-md-6.pt-5
+            .fact-box.trans
+              .fact-content
+                .circle-progress
+                  #progress1.prog-circle
+                  .progress-info
+                    h2 100%
+                  .prog-title
+                    h3 Коммуникабельна
+                    p Найду общий язык с бомжом и Папой Римским
+          .col-lg-3.col-md-6.pt-5
+            .fact-box.trans
+              .fact-content
+                .circle-progress
+                  #progress2.prog-circle
+                  .progress-info
+                    h2 100%
+                  .prog-title
+                    h3 Великолепна
+                    p а ещё скромна
+          .col-lg-3.col-md-6
+            .fact-box
+              .fact-content
+                img(src="img/icon/1-w.png" alt="")
+                h2 14
+                p Лет опыта
+          .col-lg-3.col-md-6
+            .fact-box
+              .fact-content
+                img(src="img/icon/2-w.png" alt="")
+                h2 999
+                p Наград
 </template>
 
 <script>
