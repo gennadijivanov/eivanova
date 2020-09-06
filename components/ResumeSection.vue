badd9170eec75ef2738ef49d2531d0948638368e
givanov <givanov@aligntech.com>
2020-09-06 22:38:39 +0300

    pug resume

--- components/ResumeSection.vue
+++ components/ResumeSection.vue
@@ -1,23 +1,16 @@
-<template>
-  <section class="resume-section spad" :class="{'with-bg': withBackground}">
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-xl-7 offset-xl-2">
-          <div class="section-title">
-            <h2>{{ title }}</h2>
-          </div>
-          <ul class="resume-list">
-            <li v-for="item in items" :key="item.period">
-              <h2>{{ item.period }}</h2>
-              <h3>{{ item.workplace }}</h3>
-              <h4>{{ item.occupation }}</h4>
-              <p>{{ item.description }}</p>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  </section>
+<template lang="pug">
+section.resume-section.spad(:class="{'with-bg': withBackground}")
+  .container-fluid
+    .row
+      .col-xl-7.offset-xl-2
+        .section-title
+          h2 {{ title }}
+        ul.resume-list
+          li(v-for="item in items" :key="item.period")
+            h2 {{ item.period }}
+            h3 {{ item.workplace }}
+            h4 {{ item.occupation }}
+            p {{ item.description }}
 </template>
 
 <script>
