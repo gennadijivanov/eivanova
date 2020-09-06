3526b3492ea1706cb5427142a697af8a8801d8a6
givanov <givanov@aligntech.com>
2020-09-06 22:34:02 +0300

    pug social

--- components/SocialLinks.vue
+++ components/SocialLinks.vue
@@ -1,32 +1,24 @@
-<template>
-  <div class="social-section">
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-xl-10 offset-xl-1">
-          <div class="social-link-warp">
-            <div class="social-links">
-              <!-- <a href>
-                <i class="fa fa-pinterest"></i>
-              </a>
-              <a href>
-                <i class="fa fa-linkedin"></i>
-              </a> -->
-              <a href="https://www.instagram.com/elena.ivanova.rielter/" target="__blank">
-                <i class="fa fa-instagram"></i>
-              </a>
-              <a href="https://www.facebook.com/profile.php?id=100001570811983" target="__blank">
-                <i class="fa fa-facebook"></i>
-              </a>
-              <!-- <a href>
-                <i class="fa fa-twitter"></i>
-              </a> -->
-            </div>
-            <h2 class="hidden-md hidden-sm">Социальные сети</h2>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+<template lang="pug">
+.social-section
+  .container-fluid
+    .row
+      .col-xl-10.offset-xl-1
+        .social-link-warp
+          .social-links
+            a(href="https://www.instagram.com/elena.ivanova.rielter/" target="__blank")
+              i.fa.fa-instagram
+            a(href="https://www.facebook.com/profile.php?id=100001570811983" target="__blank")
+              i.fa.fa-facebook
+              //- <a href>
+              //- <i class="fa fa-pinterest"></i>
+              //- </a>
+              //- <a href>
+              //- <i class="fa fa-linkedin"></i>
+              //- </a>
+              //- <a href>
+              //- <i class="fa fa-twitter"></i>
+              //- </a>
+          h2.hidden-md.hidden-sm Социальные сети
 </template>
 
 <style scoped>
